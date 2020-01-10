f68a1aed569cb328a29df44ad6723120e281d13e
Adam <adammellal@hotmail.com>
2020-01-10 11:03:50 +0000

    Added heroku stuff

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -15,6 +16,14 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/questions', require('./routes/api/questions'));
 
-const PORT = process.env.PORT || 5000;
+// Serve static assets in production
+if(process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static('client/build'));
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.htmll'));
+    })
+}
+
+const PORT = process.env.PORT || 5000;
